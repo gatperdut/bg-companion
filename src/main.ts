1c81be5c1bd82704653ac8c7b389288b05d4be91
gatperdut <carlos.romero.breto@gmail.com>
2025-01-31 07:40:41 +0100

    fouund process

--- src/main.ts
+++ src/main.ts
@@ -7,51 +7,51 @@ sourceMapSupport.install();
 
 
 function main(): void {
-  const win = new QMainWindow();
-  win.setWindowTitle("Hello World");
+  // const win = new QMainWindow();
+  // win.setWindowTitle("Hello World");
 
-  const centralWidget = new QWidget();
+  // const centralWidget = new QWidget();
 
-  const rootLayout = new QBoxLayout(Direction.TopToBottom);
-  centralWidget.setObjectName("myroot");
-  centralWidget.setLayout(rootLayout);
+  // const rootLayout = new QBoxLayout(Direction.TopToBottom);
+  // centralWidget.setObjectName("myroot");
+  // centralWidget.setLayout(rootLayout);
 
-  const label = new QLabel();
-  label.setObjectName("mylabel");
-  label.setText("Hello");
+  // const label = new QLabel();
+  // label.setObjectName("mylabel");
+  // label.setText("Hello");
 
-  const button = new QPushButton();
-  button.setIcon(new QIcon(path.join(__dirname, '../assets/logox200.png')));
+  // const button = new QPushButton();
+  // button.setIcon(new QIcon(path.join(__dirname, '../assets/logox200.png')));
 
-  const label2 = new QLabel();
-  label2.setText("World");
-  label2.setInlineStyle(`
-    color: red;
-  `);
+  // const label2 = new QLabel();
+  // label2.setText("World");
+  // label2.setInlineStyle(`
+  //   color: red;
+  // `);
 
-  rootLayout.addWidget(label);
-  rootLayout.addWidget(button);
-  rootLayout.addWidget(label2);
-  win.setCentralWidget(centralWidget);
-  win.setStyleSheet(
-  `
-    #myroot {
-      background-color: #009688;
-      height: '100%';
-      align-items: 'center';
-      justify-content: 'center';
-    }
-    #mylabel {
-      font-size: 16px;
-      font-weight: bold;
-      padding: 1;
-    }
-  `
-  );
-  win.show();
-
-  (global as any).win = win;
-
+  // rootLayout.addWidget(label);
+  // rootLayout.addWidget(button);
+  // rootLayout.addWidget(label2);
+  // win.setCentralWidget(centralWidget);
+  // win.setStyleSheet(
+  // `
+  //   #myroot {
+  //     background-color: #009688;
+  //     height: '100%';
+  //     align-items: 'center';
+  //     justify-content: 'center';
+  //   }
+  //   #mylabel {
+  //     font-size: 16px;
+  //     font-weight: bold;
+  //     padding: 1;
+  //   }
+  // `
+  // );
+  
+  // win.show();
+  
+  // (global as any).win = win;
   mem();
 }
 main();
--- src/mem.ts
+++ src/mem.ts
@@ -1,27 +1,104 @@
 import koffi from 'koffi/indirect';
 
-export const mem = (): void => {
-    // Load the shared library
-    const lib = koffi.load('user32.dll');
+const lib = koffi.load('kernel32.dll');
 
-    // Declare constants
-    const MB_OK = 0x0;
-    const MB_YESNO = 0x4;
-    const MB_ICONQUESTION = 0x20;
-    const MB_ICONINFORMATION = 0x40;
-    const IDOK = 1;
-    const IDYES = 6;
-    const IDNO = 7;
+const TH32CS_SNAPPROCESS = 0x2
 
-    // Find functions
-    const MessageBoxA = lib.func('__stdcall', 'MessageBoxA', 'int', ['void *', 'str', 'str', 'uint']);
-    const MessageBoxW = lib.func('__stdcall', 'MessageBoxW', 'int', ['void *', 'str16', 'str16', 'uint']);
+const PROCESSENTRY32 = koffi.struct('PROCESSENTRY32', {
+    dwSize: 'uint32',
+    cntUsage: 'uint32',
+    th32ProcessID: 'uint32',
+    th32DefaultHeapID: 'ulong *',
+    th32ModuleID: 'uint32',
+    cntThreads: 'uint32',
+    th32ParentProcessID: 'uint32',
+    pcPriClassBase: 'long',
+    dwFlags: 'uint32',
+    szExeFile: koffi.array('char', 260, 'Array')
+})
 
-    let ret = MessageBoxA(null, 'Do you want another message box?', 'Koffi', MB_YESNO | MB_ICONQUESTION);
+const CreateToolhelp32Snapshot = lib.func('__stdcall', 'CreateToolhelp32Snapshot', 'void *', ['uint32', 'void *'])
 
-    if (ret == IDYES)
-        MessageBoxW(null, 'Hello World!', 'Koffi', MB_ICONINFORMATION);
+// const Process32First = lib.func('__stdcall', 'Process32First', 'bool', ['void *', PROCESSENTRY32])
+const Process32First = lib.func('bool __stdcall Process32First(_In_ void *hSnapshot, _Inout_ PROCESSENTRY32 *lppe)');
 
+// const Process32Next = lib.func('__stdcall', 'Process32Next', 'bool', ['void *', PROCESSENTRY32])
+const Process32Next = lib.func('bool __stdcall Process32Next(_In_ void *hSnapshot, _Inout_ PROCESSENTRY32 *lppe)');
 
+// int __stdcall GetCursorPos(_Out_ POINT *pos)'
 
+const GetLastError = lib.func('__stdcall', 'GetLastError', 'uint32', []);
+
+const CloseHandle = lib.func('__stdcall', 'CloseHandle', 'bool', ['void *']);
+
+const checkError = (): number => {
+    const error = GetLastError();
+
+    console.error('ERROR: ', error);
+
+    return error;
 }
+
+export const mem = (): void => {
+    checkError();
+
+    let snapHandle = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+    console.log(snapHandle);
+
+    checkError();
+
+    let entry = {
+        dwSize: koffi.sizeof(PROCESSENTRY32),
+        cntUsage: 0,
+        th32ProcessID: 0,
+        th32DefaultHeapID: 0,
+        th32ModuleID: 0,
+        cntThreads: 0,
+        th32ParentProcessID: 0,
+        pcPriClassBase: 0,
+        dwFlags: 0,
+        szExeFile: new Array(260).fill(0)
+    };
+
+    console.log('SIZE TYPE', koffi.sizeof(PROCESSENTRY32));
+
+    console.log('SIZE OBJ', sizeOf(entry))
+
+    Process32First(snapHandle, entry);
+    
+    
+    do {
+        if (exeName(entry.szExeFile) === 'Baldur.exe') {
+            console.log('FOUND');
+        };
+    } while (Process32Next(snapHandle, entry));
+
+    CloseHandle(snapHandle);
+}
+
+const exeName = (nums: number[]): string => {
+    let result: string[] = [];
+
+    let i = 0;
+
+    while (i < nums.length && nums[i]) {
+        result.push(String.fromCharCode(nums[i]));
+
+        i++;
+    }
+
+    return result.join('');
+}
+
+const typeSizes = {
+    "undefined": () => 0,
+    "boolean": () => 4,
+    "number": () => 8,
+    "string": item => 2 * item.length,
+    "object": item => !item ? 0 : Object
+      .keys(item)
+      .reduce((total, key) => sizeOf(key) + sizeOf(item[key]) + total, 0)
+  };
+  
+  const sizeOf = value => typeSizes[typeof value](value);
